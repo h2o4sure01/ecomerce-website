cda22324d09a0853f332906fdf0a82401cac9d51
h2o4sure01 <h2o4sure01@gmail.com>
2020-01-30 12:42:26 +0100

    my shop

---
+++ shop/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    uploaded = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('title',)
+        index_together =(('id', 'slug'),)
+
+    def __str__(self):
+        return self.title
+        
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args= [self.id, self.slug])
