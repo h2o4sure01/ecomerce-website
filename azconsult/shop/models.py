892df86a1be66091eac345ef82423afe86451897
h2o4sure01 <h2o4sure01@gmail.com>
2020-01-30 15:45:49 +0100

    created the catalog session

---
+++ azconsult/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
