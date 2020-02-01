c1368e9b7a22908378a0c541fb374589680b46a3
h2o4sure01 <h2o4sure01@gmail.com>
2020-02-01 13:38:09 +0100

    added the views.py

--- azconsult/shop/admin.py
+++ azconsult/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'price', 'available', 'uploaded', 'updated']
+    list_filter = [ 'available', 'uploaded', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('title',)}
